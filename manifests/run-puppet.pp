9d1c8cd3d17d764e184732f7797503c3a1f5e26b
Fernando Garcia <fernando@cutre.net>
2018-02-07 00:45:40 +0100

    corrected path

--- manifests/run-puppet.pp
+++ manifests/run-puppet.pp
@@ -1,6 +1,6 @@
 # Set up regular Puppet runs
 file { '/usr/local/bin/run-puppet':
-  source => '/Users/fgarcia/Documents/GitHub/puppet-test/manifests',
+  source => '../files/run-puppet.sh',
   mode   => '0755',
 }
 
