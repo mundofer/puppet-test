4e347b7383a9f3eab19eeb79c25f131772b78bdf
Fernando Garcia <fernando@cutre.net>
2018-02-11 19:35:16 +0100

    added class run-puppet

--- manifests/run-puppet.pp
+++ manifests/run-puppet.pp
@@ -1,12 +1,14 @@
-# Set up regular Puppet runs
-file { '/usr/local/bin/run-puppet.sh':
-  ensure => present,
-  source => 'puppet:///modules/puppet-test/run-puppet.sh',
-  mode   => '0755',
-}
+class run-puppet {
+	# Set up regular Puppet runs
+	file { '/usr/local/bin/run-puppet.sh':
+	  ensure => present,
+	  source => 'puppet:///modules/run-puppet/run-puppet.sh',
+	  mode   => '0755',
+	}
 
-cron { 'run-puppet':
-  command => '/usr/local/bin/run-puppet.sh',
-  hour    => '*',
-  minute  => '*/15',
+	cron { 'run-puppet':
+	  command => '/usr/local/bin/run-puppet.sh',
+	  hour    => '*',
+	  minute  => '*/15',
+	}
 }
