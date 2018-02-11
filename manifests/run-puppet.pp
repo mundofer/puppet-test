0cd38ceaa04596493906d4a8820c5799a675be15
Fernando Garcia <fernando@cutre.net>
2018-02-11 19:47:46 +0100

    using module_name var with double quote

--- manifests/run-puppet.pp
+++ manifests/run-puppet.pp
@@ -1,14 +1,12 @@
-class runpuppet {
-	# Set up regular Puppet runs
-	file { '/usr/local/bin/run-puppet.sh':
-	  ensure => present,
-	  source => 'puppet:///modules/run-puppet/run-puppet.sh',
-	  mode   => '0755',
-	}
+# Set up regular Puppet runs
+file { '/usr/local/bin/run-puppet.sh':
+  ensure => present,
+  source => "puppet:///modules/($module-name)/run-puppet.sh",
+  mode   => '0755',
+}
 
-	cron { 'run-puppet':
-	  command => '/usr/local/bin/run-puppet.sh',
-	  hour    => '*',
-	  minute  => '*/15',
-	}
+cron { 'run-puppet':
+  command => '/usr/local/bin/run-puppet.sh',
+  hour    => '*',
+  minute  => '*/15',
 }
