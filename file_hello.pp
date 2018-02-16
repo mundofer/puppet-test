2f992d0a63171ed452afdf70a6124364ef978600
Fernando Garcia <fernando@cutre.net>
2018-02-16 23:42:02 +0100

    gorditoW

--- file_hello.pp
+++ file_hello.pp
@@ -2,5 +2,5 @@ file { '/tmp/hello.txt':
   ensure  => file,
   content => "#This file manager by puppet.
 # Any manual edit will be lost.
-Hola, mundo redondito\n",
+Hola, mundo redondito y gordito\n",
 }
