2ad22b60ccc5d6eb01cf4654b249bc24f912adc0
Fernando Garcia <fernando@cutre.net>
2018-02-18 18:04:44 +0100

    changed repo directory to our repo

--- manifests/run-puppet.pp
+++ manifests/run-puppet.pp
@@ -1,7 +1,7 @@
 # Set up regular Puppet runs
 file { '/usr/local/bin/run-puppet.sh':
   ensure => present,
-  content => "#!/bin/bash\ncd /etc/puppetlabs/code/environments/production && git pull\n/opt/puppetlabs/bin/puppet apply manifests/\n",
+  content => "#!/bin/bash\ncd /home/vagrant/puppet && git pull\n/opt/puppetlabs/bin/puppet apply manifests/\n",
   #source => "puppet:///modules/run-puppet.sh",
   mode   => '0755',
 }
