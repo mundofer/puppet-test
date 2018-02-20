3db685b320a631cdc3a218adc00910d6a96c455c
Fernando Garcia <fernando@cutre.net>
2018-02-20 20:58:07 +0100

    added ssh key for vagrant

---
+++ manifests/ssh-keys.pp
@@ -0,0 +1,5 @@
+ssh_authorized_key { 'fgarcia@MacBook-Pro-de-it.local':
+  user => 'vagrant',
+  type => ssh-rsa,
+  key => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDE0W0b2p7S230LeIdCtjVVYOCyznqgByzIeQ6VnLVTTJ7h0Na5aLuSJ8vvygSr9HdFzQjjUdYT5xm+8lqPhrM0IzTES7A6j6w7Ca8E2krmAMC8An1CIdMNUxWkmWChiS0HrjthVOKrF/oG/5jE6BsmMzxED/vLHo+l5ZEUK/fGys4FnUvJHw5phbMi3BGN60iFvXdNHQiZlji6c0P8X8VoDgymtqP7E1uF8C0BQKaFdIG2w1aUuOkK7D4Dqd+FoNaRCQMR1lwTw07q9cdYXptlhMhxOxPmvOyP+eOpi0KKbFun/P3fZfp4IJycI2eEXz77Xs/nBr43l1bvZeQZUhMB',
+}
